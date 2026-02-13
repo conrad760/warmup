9def0ac4f1c595a7b1c545681b4acecf218b4ce0
Conrad Parker <conradlparker@gmail.com>
2026-02-12 17:45:00 -0700

    Add Nix flake and direnv for reproducible dev environment

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "DSA warmup dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              go-tools # staticcheck
+              gopls
+              git
+              gnumake
+              util-linux # column (used by make help)
+            ];
+          };
+        });
+    };
+}
